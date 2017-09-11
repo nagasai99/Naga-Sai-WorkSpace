2b45c16bbe55df4ce63a7f768bcbef8a56e1bf7b
Naga Sai <nagasai1818@gmail.com>
2017-09-11 18:04:34 -0500

    ruby commit

---
+++ ruby/ruby-runner.c
@@ -0,0 +1,51 @@
+#define _POSIX_C_SOURCE 200809L
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+
+#include "ruby-runner.h"
+
+#define STRINGIZE(expr) STRINGIZE0(expr)
+#define STRINGIZE0(expr) #expr
+
+int
+main(int argc, char **argv)
+{
+    static const char builddir[] = BUILDDIR;
+    static const char rubypath[] = BUILDDIR"/"STRINGIZE(RUBY_INSTALL_NAME);
+    const size_t dirsize = sizeof(builddir);
+    const size_t namesize = sizeof(rubypath) - dirsize;
+    const char *rubyname = rubypath + dirsize;
+    char *arg0 = argv[0], *p;
+    const char *libpath = getenv(LIBPATHENV);
+    char c = 0;
+
+    if (libpath) {
+	while ((c = *libpath) == PATH_SEP) ++libpath;
+    }
+    if (c) {
+	size_t n = strlen(libpath);
+	char *e = malloc(dirsize+n+1);
+	memcpy(e, builddir, dirsize-1);
+	e[dirsize-1] = PATH_SEP;
+	memcpy(e+dirsize, libpath, n+1);
+	libpath = e;
+    }
+    else {
+	libpath = builddir;
+    }
+    setenv(LIBPATHENV, libpath, 1);
+
+    if (!(p = strrchr(arg0, '/'))) p = arg0; else p++;
+    if (strlen(p) < namesize - 1) {
+	argv[0] = malloc(p - arg0 + namesize);
+	memcpy(argv[0], arg0, p - arg0);
+	memcpy(argv[0] + (p - arg0), rubypath + dirsize, namesize);
+    }
+    else {
+	memcpy(p, rubyname, namesize);
+    }
+
+    execv(rubypath, argv);
+    return -1;
+}
---
+++ ruby/tool/run-lcov.rb
@@ -0,0 +1,82 @@
+#!ruby
+require "pathname"
+require "open3"
+
+def backup_gcda_files(gcda_files)
+  gcda_files = gcda_files.map do |gcda|
+    [gcda, gcda.sub_ext(".bak")]
+  end
+  begin
+    gcda_files.each do |before, after|
+      before.rename(after)
+    end
+    yield
+  ensure
+    gcda_files.each do |before, after|
+      after.rename(before)
+    end
+  end
+end
+
+$info_files = []
+def run_lcov(dir, info)
+  $info_files << info
+  system("lcov", "-c", "-d", dir, "--rc", "lcov_branch_coverage=1", "-o", info)
+end
+
+def gen_rb_lcov(file)
+  res = Marshal.load(File.binread(file))
+
+  open("lcov-rb-all.info", "w") do |f|
+    f.puts "TN:" # no test name
+    base_dir = File.dirname(__dir__)
+    res.each do |path, cov|
+      next unless path.start_with?(base_dir)
+      next if path.start_with?(File.join(base_dir, "test"))
+      f.puts "SF:#{ path }"
+
+      total = covered = 0
+      cov.each_with_index do |count, lineno|
+        next unless count
+        f.puts "DA:#{ lineno + 1 },#{ count }"
+        total += 1
+        covered += 1 if count > 0
+      end
+      f.puts "LF:#{ total }"
+      f.puts "LH:#{ covered }"
+
+      f.puts "end_of_record"
+    end
+  end
+end
+
+gcda_files = Pathname.glob("**/*.gcda")
+ext_gcda_files = gcda_files.select {|f| f.fnmatch("ext/*") }
+rubyspec_temp_gcda_files = gcda_files.select {|f| f.fnmatch("rubyspec_temp/*") }
+
+backup_gcda_files(rubyspec_temp_gcda_files) do
+  if ext_gcda_files != []
+    backup_gcda_files(ext_gcda_files) do
+      info = "lcov-root.info"
+      run_lcov(".", info)
+    end
+  end
+  ext_gcda_files.group_by {|f| f.descend.to_a[1] }.each do |key, files|
+    info = "lcov-#{ key.to_s.gsub(File::Separator, "-") }.info"
+    run_lcov(key.to_s, info)
+  end
+end
+if $info_files != []
+  system("lcov", *$info_files.flat_map {|f| ["-a", f] }, "-o", "lcov-c-all.info")
+  system("genhtml", "--ignore-errors", "source", "lcov-c-all.info", "-o", "lcov-c-out")
+end
+
+if File.readable?("test-coverage.dat")
+  gen_rb_lcov("test-coverage.dat")
+  system("genhtml", "--ignore-errors", "source", "lcov-rb-all.info", "-o", "lcov-rb-out")
+end
+
+if File.readable?("lcov-c-all.info") && File.readable?("lcov-rb-all.info")
+  system("lcov", "-a", "lcov-c-all.info", "-a", "lcov-rb-all.info", "-o", "lcov-all.info") || raise
+  system("genhtml", "--ignore-errors", "source", "lcov-all.info", "-o", "lcov-out")
+end
